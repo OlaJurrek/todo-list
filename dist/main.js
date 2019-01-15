0353b88faa8d4bd549f3eae17ce1d1774ecfaffe
Aleksandra Jurek <jurek.aleksandra@gmail.com>
2019-01-15 21:55:41 +0100

    add function randomquotes

--- dist/main.js
+++ dist/main.js
@@ -47,6 +47,7 @@ function showPage() {
     addNewBtn.classList.add("move-btn");
     addNewBtn.firstElementChild.textContent = "GO";
     addNewBtn.lastElementChild.textContent = "BACK";
+    randomQuote();
   } else {
     addNewPage.classList.remove("show-page");
     addNewBtn.classList.remove("move-btn");
@@ -104,4 +105,75 @@ function deleteOne(e) {
   if (e.target.parentElement.className == "delete-task") {
     e.target.parentElement.parentElement.remove();
   }
+}
+
+// Random quotes from hell
+const quotes = [
+  {
+    author: "Zig Ziglar",
+    quote:
+      "Lack of direction, not lack of time, is the problem. We all have twenty-four hour days."
+  },
+  {
+    author: "Alexander Graham Bell",
+    quote:
+      "Concentrate all your thoughts upon the work at hand. The sun’s rays do not burn until brought to a focus."
+  },
+  {
+    author: "Will Durant",
+    quote:
+      "We are what we repeatedly do. Excellence, then, is not an act but a habit."
+  },
+  {
+    author: "Bruce Lee",
+    quote: "The successful warrior is the average man, with laser-like focus."
+  },
+  {
+    author: "Denis Waitley",
+    quote:
+      "Don’t dwell on what went wrong. Instead, focus on what to do next. Spend your energies on moving forward toward finding the answer."
+  },
+  {
+    author: "Mike Hawkins",
+    quote:
+      "You don’t get results by focusing on results. You get results by focusing on the actions that produce results."
+  },
+  {
+    author: "Brian Tracy",
+    quote:
+      "A clear vision, backed by definite plans, gives you a tremendous feeling of confidence and personal power."
+  },
+  {
+    author: "Bill Gates",
+    quote:
+      "My success, part of it certainly, is that I have focused in on a few things."
+  },
+  {
+    author: "Stuart Wilde",
+    quote:
+      "Discovering what you really want saves you endless confusion and wasted energy."
+  },
+  {
+    author: "Zen proverb",
+    quote: "When walking, walk. When eating, eat."
+  },
+  {
+    author: "John Carmack",
+    quote: "Focus is a matter of deciding what things you’re not going to do."
+  },
+  {
+    author: "James Caviezel",
+    quote:
+      "I do what I do, and I do it well, and focus and take it one moment at a time."
+  }
+];
+
+const quoteBox = document.querySelector("blockquote");
+const cite = document.querySelector("cite");
+
+function randomQuote() {
+  const quotesAmount = quotes.length;
+  const randomNumber = Math.floor(Math.random() * quotesAmount);
+  quoteBox.textContent = quotes[randomNumber].quote;
+  cite.textContent = quotes[randomNumber].author;
 }
