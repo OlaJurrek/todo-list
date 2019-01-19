3959aae8e9fe26af6258ac437bb02f2e86567d89
Aleksandra Jurek <jurek.aleksandra@gmail.com>
2019-01-19 18:05:55 +0100

    add mark complete functions

--- dist/main.js
+++ dist/main.js
@@ -1,5 +1,7 @@
 //Define UI variables
 const currentTimeBox = document.querySelector(".current-time");
+const taskCounter = document.querySelector(".todo-counter");
+const completedCounter = document.querySelector(".done-counter");
 const addNewBtn = document.querySelector(".add-new-btn");
 const addNewPage = document.querySelector(".add-new-page");
 const taskList = document.querySelector(".task-list");
@@ -8,10 +10,53 @@ const addTaskBtn = document.querySelector('input[type="submit"]');
 const addNewForm = document.querySelector(".input-field form");
 const filterInput = document.querySelector("#filter");
 const deleteBtn = document.querySelector(".delete-btn");
+const showCompletedBtn = document.querySelector(".show-completed-btn");
+const completedListPage = document.querySelector(".completed-list-page");
+const completedTaskList = document.querySelector(".completed-list");
+const exitBtn = document.querySelector(".exit-btn");
+const markAllBtn = document.querySelector(".all-complete-btn");
 
 // Show current time
 const today = new Date();
 currentTimeBox.innerHTML = "Today is " + today.toLocaleDateString("pl-PL");
+
+// Show tasks stored in LS
+document.addEventListener("DOMContentLoaded", showTasks);
+
+function showTasks() {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+  tasks.forEach(function(task) {
+    const newTask = document.createElement("li");
+    newTask.className = "task";
+    newTask.appendChild(document.createTextNode(task));
+    const markCompleted = document.createElement("a");
+    markCompleted.className = "mark-complete";
+    markCompleted.innerHTML = '<i class="far fa-check-circle"></i>';
+    newTask.appendChild(markCompleted);
+    const deleteTask = document.createElement("a");
+    deleteTask.className = "delete-task";
+    deleteTask.innerHTML = '<i class="far fa-times-circle"></i>';
+    newTask.appendChild(deleteTask);
+    taskList.appendChild(newTask);
+  });
+  countTasks();
+}
+
+// Show number of tasks to do
+function countTasks() {
+  taskCounter.innerHTML = taskList.children.length;
+}
+
+// Show number of completed tasks
+function countCompleted() {
+  // let do completed list first!
+  completedCounter.innerHTML = completedTaskList.children.length;
+}
 
 // Label goes up if the input is focused
 document.body.addEventListener("focusin", labelUp);
@@ -54,6 +99,22 @@ function showPage() {
   }
 }
 
+// Show completed list page
+showCompletedBtn.addEventListener("click", showPage2);
+
+function showPage2() {
+  if (!completedListPage.className.includes("show-page")) {
+    completedListPage.classList.add("show-page");
+  }
+}
+
+// Hide completed list page
+exitBtn.addEventListener("click", hidePage);
+
+function hidePage() {
+  completedListPage.classList.remove("show-page");
+}
+
 // Add new task
 addNewForm.addEventListener("submit", addTask);
 
@@ -76,9 +137,27 @@ function addTask(e) {
   deleteTask.innerHTML = '<i class="far fa-times-circle"></i>';
   newTask.appendChild(deleteTask);
   taskList.appendChild(newTask);
+  // Add to LS
+  addToLocalStorage(taskInput.value);
+  // Change counter
+  countTasks();
+  // Clear input field
   taskInput.value = "";
+  // Go back to main page
   showPage();
   e.preventDefault();
+}
+
+// Add new task to LocalStorage
+function addToLocalStorage(task) {
+  let tasks;
+  if (localStorage.getItem("tasks") === null) {
+    tasks = [];
+  } else {
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+  }
+  tasks.push(task);
+  localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
 // Filter tasks
@@ -101,6 +180,8 @@ function deleteAll() {
   while (taskList.firstChild) {
     taskList.lastChild.remove();
   }
+  localStorage.removeItem("tasks");
+  countTasks();
 }
 
 //Delete specific one
@@ -108,8 +189,92 @@ taskList.addEventListener("click", deleteOne);
 
 function deleteOne(e) {
   if (e.target.parentElement.className == "delete-task") {
-    e.target.parentElement.parentElement.remove();
+    let deleteMe = e.target.parentElement.parentElement;
+    removeTaskFromLocalStorage(deleteMe.textContent);
+    deleteMe.remove();
+    countTasks();
   }
+}
+
+// Remove one task from LS
+function removeTaskFromLocalStorage(taskToRemove) {
+  const tasks = JSON.parse(localStorage.getItem("tasks"));
+  tasks.forEach(function(task) {
+    if (task == taskToRemove) {
+      tasks.splice(tasks.indexOf(task), 1);
+    }
+  });
+  localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+// Mark all tasks as complete
+markAllBtn.addEventListener("click", markAllComplete);
+
+function markAllComplete() {
+  // check if there is sth in LS
+  let completedTasks;
+  if (localStorage.getItem("completedTasks") === null) {
+    completedTasks = [];
+  } else {
+    completedTasks = JSON.parse(localStorage.getItem("completedTasks"));
+  }
+
+  completedTasks.concat(JSON.parse(localStorage.getItem("tasks")));
+  deleteAll();
+  showCompletedTasks();
+  countCompleted();
+}
+
+// Mark one task as complete
+taskList.addEventListener("click", markOneComplete);
+
+function markOneComplete(e) {
+  if (e.target.parentElement.className == "mark-complete") {
+    let completedTask = e.target.parentElement.parentElement;
+    removeTaskFromLocalStorage(completedTask.textContent);
+    let completedTasks;
+    if (localStorage.getItem("completedTasks") === null) {
+      completedTasks = [];
+    } else {
+      completedTasks = JSON.parse(localStorage.getItem("completedTasks"));
+    }
+
+    completedTasks.push(completedTask.textContent);
+    localStorage.setItem("completedTasks", JSON.stringify(completedTasks));
+    completedTask.remove();
+    showCompletedTasks();
+    countTasks();
+    countCompleted();
+  }
+}
+
+// Show completed task on completed task page
+document.addEventListener("DOMContentLoaded", showCompletedTasks);
+
+function showCompletedTasks() {
+  // check if there is sth in LS
+  let completedTasks;
+  if (localStorage.getItem("completedTasks") === null) {
+    completedTasks = [];
+  } else {
+    completedTasks = JSON.parse(localStorage.getItem("completedTasks"));
+  }
+
+  completedTasks.forEach(function(task) {
+    const newTask = document.createElement("li");
+    newTask.className = "task";
+    newTask.appendChild(document.createTextNode(task));
+    // const markCompleted = document.createElement("a");
+    // markCompleted.className = "mark-complete";
+    // markCompleted.innerHTML = '<i class="far fa-check-circle"></i>';
+    // newTask.appendChild(markCompleted);
+    // const deleteTask = document.createElement("a");
+    // deleteTask.className = "delete-task";
+    // deleteTask.innerHTML = '<i class="far fa-times-circle"></i>';
+    // newTask.appendChild(deleteTask);
+    completedTaskList.appendChild(newTask);
+  });
+  countCompleted();
 }
 
 // Random quotes from hell
