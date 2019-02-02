34554075232463e1859514973c01dae95995b304
Aleksandra Jurek <jurek.aleksandra@gmail.com>
2019-02-02 23:12:36 +0100

    Amend displaying completed task functions

--- dist/main.js
+++ dist/main.js
@@ -1,12 +1,8 @@
-//Define UI variables
-const currentTimeBox = document.querySelector(".current-time");
-const taskCounter = document.querySelector(".todo-counter");
-const completedCounter = document.querySelector(".done-counter");
+//Define main UI variables
 const newTaskBtn = document.querySelector(".new-task-btn");
 const addNewPage = document.querySelector(".add-new-page");
 const taskList = document.querySelector(".todo-list");
 const taskInput = document.querySelector("#new-task");
-// const addTaskBtn = document.querySelector('input[type="submit"]');
 const addTaskBtn = document.querySelector("button.round-btn");
 const addNewForm = document.querySelector(".input-field form");
 const filterInput = document.querySelector("#filter");
@@ -14,9 +10,9 @@ const deleteBtn = document.querySelector(".delete-btn");
 const showCompletedBtn = document.querySelector(".show-completed-btn");
 const completedListPage = document.querySelector(".completed-list-page");
 const completedTaskList = document.querySelector(".completed-list");
-const markAllBtn = document.querySelector(".all-complete-btn");
 
 // Show current time
+const currentTimeBox = document.querySelector(".current-time");
 const today = new Date();
 currentTimeBox.innerHTML = "Today is " + today.toLocaleDateString("pl-PL");
 
@@ -24,37 +20,32 @@ currentTimeBox.innerHTML = "Today is " + today.toLocaleDateString("pl-PL");
 document.addEventListener("DOMContentLoaded", showTasks);
 
 function showTasks() {
+  // Check if there are any tasks in LS
   let tasks;
   if (localStorage.getItem("tasks") === null) {
     tasks = [];
   } else {
     tasks = JSON.parse(localStorage.getItem("tasks"));
   }
-  tasks.forEach(function(task) {
-    const newTask = document.createElement("li");
-    newTask.className = "task";
-    newTask.appendChild(document.createTextNode(task));
-    const markCompleted = document.createElement("a");
-    markCompleted.className = "mark-complete";
-    markCompleted.innerHTML = '<i class="far fa-check-circle"></i>';
-    newTask.appendChild(markCompleted);
-    const deleteTask = document.createElement("a");
-    deleteTask.className = "delete-task";
-    deleteTask.innerHTML = '<i class="far fa-times-circle"></i>';
-    newTask.appendChild(deleteTask);
-    taskList.appendChild(newTask);
-  });
+  // Display all tasks in UI
+  for (const task of tasks) {
+    createTaskInUI(task, taskList, "new");
+  }
+  // Update new task counter
   countTasks();
 }
 
 // Show number of tasks to do
+const taskCounter = document.querySelector(".todo-counter");
+
 function countTasks() {
   taskCounter.innerHTML = taskList.children.length;
 }
 
 // Show number of completed tasks
+const completedCounter = document.querySelector(".done-counter");
+
 function countCompleted() {
-  // let do completed list first!
   completedCounter.innerHTML = completedTaskList.children.length;
 }
 
@@ -87,14 +78,13 @@ newTaskBtn.addEventListener("click", showPage);
 function showPage() {
   if (!addNewPage.className.includes("show-page")) {
     addNewPage.classList.add("show-page");
-    // showCompletedBtn.style.zIndex = 2;
     addTaskBtn.style.zIndex = 5;
     newTaskBtn.firstElementChild.textContent = "GO";
     newTaskBtn.lastElementChild.textContent = "BACK";
+    // Display random quote
     randomQuote();
   } else {
     addNewPage.classList.remove("show-page");
-    // showCompletedBtn.style.zIndex = 5;
     addTaskBtn.style.zIndex = -1;
     newTaskBtn.firstElementChild.textContent = "NEW";
     newTaskBtn.lastElementChild.textContent = "TASK";
@@ -119,27 +109,36 @@ function showPage2() {
 }
 
 // Add new task
-addNewForm.addEventListener("submit", addTask);
+addNewForm.addEventListener("submit", addNewTask);
 
-function addTask(e) {
+function createTaskInUI(value, list, status = "completed") {
+  // Create new li element
+  const task = document.createElement("li");
+  task.className = "task";
+  task.appendChild(document.createTextNode(value));
+  if (status === "new") {
+    // Create and add completed icon
+    const markCompleted = document.createElement("a");
+    markCompleted.className = "mark-complete";
+    markCompleted.innerHTML = '<i class="far fa-check-circle"></i>';
+    task.appendChild(markCompleted);
+  }
+  // Create and add delete icon
+  const deleteTask = document.createElement("a");
+  deleteTask.className = "delete-task";
+  deleteTask.innerHTML = '<i class="far fa-times-circle"></i>';
+  task.appendChild(deleteTask);
+  // Add li to list
+  list.appendChild(task);
+}
+
+function addNewTask(e) {
   if (taskInput.value === "") {
     alert("Add new task");
     return;
   }
-
-  const newTask = document.createElement("li");
-  newTask.className = "task";
-
-  newTask.appendChild(document.createTextNode(taskInput.value));
-  const markCompleted = document.createElement("a");
-  markCompleted.className = "mark-complete";
-  markCompleted.innerHTML = '<i class="far fa-check-circle"></i>';
-  newTask.appendChild(markCompleted);
-  const deleteTask = document.createElement("a");
-  deleteTask.className = "delete-task";
-  deleteTask.innerHTML = '<i class="far fa-times-circle"></i>';
-  newTask.appendChild(deleteTask);
-  taskList.appendChild(newTask);
+  // Add new task to UI
+  createTaskInUI(taskInput.value, taskList, "new");
   // Add to LS
   addToLocalStorage(taskInput.value);
   // Change counter
@@ -167,7 +166,7 @@ function addToLocalStorage(task) {
 filterInput.addEventListener("keyup", filterTasks);
 
 function filterTasks() {
-  for (let task of taskList.children) {
+  for (const task of taskList.children) {
     if (task.textContent.indexOf(filterInput.value) !== -1) {
       task.style.display = "block";
     } else {
@@ -211,20 +210,32 @@ function removeTaskFromLocalStorage(taskToRemove) {
 }
 
 // Mark all tasks as complete
+const markAllBtn = document.querySelector(".all-complete-btn");
+
 markAllBtn.addEventListener("click", markAllComplete);
 
 function markAllComplete() {
-  // check if there is sth in LS
+  // Check if there are any new tasks and stop executing if not
+  if (taskList.children.length === 0) return;
+  // Check if there is completed task list in LS
   let completedTasks;
   if (localStorage.getItem("completedTasks") === null) {
     completedTasks = [];
   } else {
     completedTasks = JSON.parse(localStorage.getItem("completedTasks"));
   }
-
-  completedTasks.concat(JSON.parse(localStorage.getItem("tasks")));
+  // Get the whole new task list from LS and add to completed task list
+  const justCompletedTasks = JSON.parse(localStorage.getItem("tasks"));
+  completedTasks = completedTasks.concat(justCompletedTasks);
+  // Set new completed task list to LS
+  localStorage.setItem("completedTasks", JSON.stringify(completedTasks));
+  // Clear new task list in LS
   deleteAll();
-  showCompletedTasks();
+  // Add all new tasks to completed task list in UI
+  for (const task of justCompletedTasks) {
+    createTaskInUI(task, completedTaskList);
+  }
+  // Update completed tasks counter
   countCompleted();
 }
 
@@ -245,7 +256,7 @@ function markOneComplete(e) {
     completedTasks.push(completedTask.textContent);
     localStorage.setItem("completedTasks", JSON.stringify(completedTasks));
     completedTask.remove();
-    showCompletedTasks();
+    createTaskInUI(completedTask.textContent, completedTaskList);
     countTasks();
     countCompleted();
   }
@@ -263,20 +274,10 @@ function showCompletedTasks() {
     completedTasks = JSON.parse(localStorage.getItem("completedTasks"));
   }
 
-  completedTasks.forEach(function(task) {
-    const newTask = document.createElement("li");
-    newTask.className = "task";
-    newTask.appendChild(document.createTextNode(task));
-    // const markCompleted = document.createElement("a");
-    // markCompleted.className = "mark-complete";
-    // markCompleted.innerHTML = '<i class="far fa-check-circle"></i>';
-    // newTask.appendChild(markCompleted);
-    // const deleteTask = document.createElement("a");
-    // deleteTask.className = "delete-task";
-    // deleteTask.innerHTML = '<i class="far fa-times-circle"></i>';
-    // newTask.appendChild(deleteTask);
-    completedTaskList.appendChild(newTask);
-  });
+  for (const task of completedTasks) {
+    createTaskInUI(task, completedTaskList);
+  }
+
   countCompleted();
 }
 
